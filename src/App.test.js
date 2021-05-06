2d1ef98d02592b8c028c71d3fd6ba5e3a735c365
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-06 13:22:40 -0400

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -7,13 +7,13 @@ test("On load, renders App without error", ()=> {
 });
 
 test("On load, renders the app header", ()=> {
-    //Arrange: Render out App component
+    //Arrange: Render out App component.
     render(<App/>);
 
-    //Act: get our header element
+    //Act: Get our header element.
     const header = screen.queryByText(/add new animal/i);
 
-    //Assert: Does our element exist
+    //Assert: Does our element exist?
     expect(header).toBeInTheDocument();
     expect(header).toBeTruthy();
     expect(header).toHaveTextContent(/add new animal/i);
