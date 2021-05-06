a74f29b68783b20936c81d0787daf1ddc69489d8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-06 12:52:49 -0400

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,8 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe("edge case", ()=>{
-    test("test 1", ()=> {
-        console.log("this is a test");
-    });
-    
-    test("test 2", ()=> {
-        console.log("this is a test");
-    });
-    
-    test("test 3", ()=> {
-        console.log("this is a test");
-    });
-});
 
-describe("negative cases", ()=>{
-    test("test 1", ()=> {
-        console.log("this is a test");
-    });
-    
-    test("test 2", ()=> {
-        console.log("this is a test");
-    });
-    
-    test("test 3", ()=> {
-        console.log("this is a test");
-    });
+test("test 1", ()=> {
+    console.log("this is a test");
 });
